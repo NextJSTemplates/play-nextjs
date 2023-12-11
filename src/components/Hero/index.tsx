c01449f951b06486ca9156b5e9016bedf81f4758
Musharofchy <mosarrof121@gmail.com>
2023-12-11 13:09:25 +0600

    proper linking

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -24,7 +24,7 @@ const Hero = () => {
                 <ul className="mb-10 flex flex-wrap items-center justify-center gap-5">
                   <li>
                     <Link
-                      href="https://links.tailgrids.com/play-download"
+                      href="https://nextjstemplates.com/templates/play"
                       className="inline-flex items-center justify-center rounded-md bg-white px-7 py-[14px] text-center text-base font-medium text-dark shadow-1 transition duration-300 ease-in-out hover:bg-gray-2 hover:text-body-color"
                     >
                       Download Now
