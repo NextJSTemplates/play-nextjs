478a9dde898f2153bb8c20bdd44c631d985cf201
Musharofchy <mosarrof121@gmail.com>
2023-12-11 17:18:21 +0600

    proper linking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,8 @@ import { getAllPosts } from "@/utils/markdown";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Play | Free Next.js Template for Startup and SaaS By TailGrids",
-  description: "This is Home page description",
+  title: "Play Next.js - SaaS Starter Kit and Boilerplate for Next.js",
+  description: "Free Next.js SaaS Boilerplate and Starter Kit designed and built for SaaS startups. It comes with all necessary integrations, pages, and components you need to launch a feature-rich SaaS websites.",
 };
 
 export default function Home() {
