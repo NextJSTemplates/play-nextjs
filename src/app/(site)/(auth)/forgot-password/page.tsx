a9cb66bde6bb0f7ec58d6701b8dce7e217c42c6b
coderamrin <coderamrin@gmail.com>
2024-03-19 14:13:08 +0600

    updated SEO metadata

--- src/app/(site)/(auth)/forgot-password/page.tsx
+++ src/app/(site)/(auth)/forgot-password/page.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import ForgotPassword from "@/components/Auth/ForgotPassword";
 import Breadcrumb from "@/components/Common/Breadcrumb";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Forgot Password | Play SaaS Starter Kit and Boilerplate for Next.js",
+};
 
 const ForgotPasswordPage = () => {
   return (
--- src/app/(site)/(auth)/reset-password/[token]/page.tsx
+++ src/app/(site)/(auth)/reset-password/[token]/page.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import ResetPassword from "@/components/Auth/ResetPassword";
 import Breadcrumb from "@/components/Common/Breadcrumb";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Reset Password | Play SaaS Starter Kit and Boilerplate for Next.js",
+};
 
 const ResetPasswordPage = ({ params }: { params: { token: string } }) => {
   return (
