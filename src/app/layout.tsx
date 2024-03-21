d36b03938483944db7adad0af76aa96ed46a64d2
coderamrin <coderamrin@gmail.com>
2024-03-21 13:50:17 +0600

    added preloader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,12 +8,20 @@ import { ThemeProvider } from "next-themes";
 import "../styles/index.css";
 import "../styles/prism-vsc-dark-plus.css";
 import ToasterContext from "./api/contex/ToasetContex";
+import { useEffect, useState } from "react";
+import PreLoader from "@/components/Common/PreLoader";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
   return (
     <html suppressHydrationWarning={true} className="!scroll-smooth" lang="en">
       {/*
@@ -23,19 +31,23 @@ export default function RootLayout({
       <head />
 
       <body>
-        <SessionProvider>
-          <ThemeProvider
-            attribute="class"
-            enableSystem={false}
-            defaultTheme="light"
-          >
-            <ToasterContext />
-            <Header />
-            {children}
-            <Footer />
-            <ScrollToTop />
-          </ThemeProvider>
-        </SessionProvider>
+        {loading ? (
+          <PreLoader />
+        ) : (
+          <SessionProvider>
+            <ThemeProvider
+              attribute="class"
+              enableSystem={false}
+              defaultTheme="light"
+            >
+              <ToasterContext />
+              <Header />
+              {children}
+              <Footer />
+              <ScrollToTop />
+            </ThemeProvider>
+          </SessionProvider>
+        )}
       </body>
     </html>
   );
