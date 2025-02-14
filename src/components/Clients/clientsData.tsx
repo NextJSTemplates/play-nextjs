e317d525491591ffb9b81bcf88af8895018846d7
cypha-io <cyphauser.main@gmail.com>
2025-02-14 13:09:27 +0000

    Updated logos

--- src/components/Clients/clientsData.tsx
+++ src/components/Clients/clientsData.tsx
@@ -1,54 +1,53 @@
 import { Client } from "@/types/client";
+
 export const clientsData: Client[] = [
-  
   {
-    id: 2,
-    title: "google",
-    logo: "images/brands/google.svg",
+    id: 1,
+    title: "Google",
+    logo: "/images/brands/google.svg",
     logoWhite: "/images/brands/google.svg",
     link: "#",
   },
   {
     id: 2,
     title: "AT Lab",
-    logo: "images/brands/at.svg",
+    logo: "/images/brands/at.svg",
     logoWhite: "/images/brands/at.svg",
     link: "#",
   },
   {
-    id: 2,
+    id: 3,
     title: "CDLI Lab",
-    logo: "images/brands/cdli.svg",
+    logo: "/images/brands/cdli.svg",
     logoWhite: "/images/brands/cdli.svg",
     link: "#",
   },
   {
-    id: 2,
+    id: 4,
     title: "HCL Lab",
-    logo: "images/brands/ucl.svg",
+    logo: "/images/brands/ucl.svg",
     logoWhite: "/images/brands/ucl.svg",
     link: "#",
   },
   {
-    id: 2,
-    title: "HCL Lab",
-    logo: "images/brands/uk-aid.svg",
+    id: 5,
+    title: "UK Aid",
+    logo: "/images/brands/uk-aid.svg",
     logoWhite: "/images/brands/uk-aid.svg",
     link: "#",
   },
   {
-    id: 3,
+    id: 6,
     title: "GDIH",
     logo: "/images/brands/uog.svg",
     logoWhite: "/images/brands/uog.svg",
     link: "#",
   },
   {
-    id: 4,
-    title: "google",
+    id: 7,
+    title: "Google",
     logo: "/images/brands/gdih.svg",
     logoWhite: "/images/brands/gdih.svg",
     link: "#",
   },
-  
 ];
