e8572ae3840df185a44601cc2405c9faad0e1829
Naim Uddin <naim.pimjo@gmail.com>
2025-12-03 15:42:17 +0600

    update prisma lts version

--- src/utils/prismaDB.ts
+++ src/utils/prismaDB.ts
@@ -1,17 +1,16 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client"
+import { PrismaPg } from '@prisma/adapter-pg'
 
-// PrismaClient is attached to the `global` object in development to prevent
-// exhausting your database connection limit.
-//
-// Learn more:
-// https://pris.ly/d/help/next-js-best-practices
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient
+}
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+})
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["query"],
-  });
+export const prisma = globalForPrisma.prisma || new PrismaClient({
+  adapter,
+})
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
